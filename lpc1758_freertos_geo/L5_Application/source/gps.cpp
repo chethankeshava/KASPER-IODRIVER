17ac29b912e5bcee5cf25756f3abd9dfff43bb72
Ankit Gandhi <ankit4970@gmail.com>
2016-10-25 20:38:36 -0700

    Added geo_controlelr folder

---
+++ lpc1758_freertos_geo/L5_Application/source/gps.cpp
@@ -0,0 +1,34 @@
+/*
+ * gps.cpp
+ *
+ *  Created on: Oct 17, 2016
+ *      Author: ankit
+ */
+#include "tasks.hpp"
+#include "uart2.hpp"
+
+#if 0
+class gps_task : public scheduler_task
+{
+	public:
+	Uart2 &gps_uart;
+	gps_task(uint8_t priority) :
+	            scheduler_task("gps_task", 2000, priority)
+		{
+
+			/* Nothing to init */
+		}
+		bool init(void)
+		{
+
+			return true;
+		}
+
+		bool run(void *p)
+		{
+			return true;
+		}
+};
+*/
+#endif
+
